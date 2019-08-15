84f1a81873eed855d4d05f59c3ea0712b01303b6
mmboma2vm <mmboma2@illinois.edu>
2019-08-15 09:28:12 -0700

    Adline

--- SimplePerceptron/SimplePerceptron.py
+++ SimplePerceptron/SimplePerceptron.py
@@ -4,6 +4,73 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
+class AdalineGD(object):
+	""" Adapative LInear NEuron classifier.
+
+	Parameters
+	----------
+	eta: float
+		learning rate (between 0.0 and 1.0)
+	n_iter: int
+		Passes over the training dataset.
+	random_state: int
+		Random number generator seed for random weight initialization
+
+	Attributes
+	----------
+	w_ : 1d-array
+		weights after fitting
+	cost_ : list
+		sum of squares cost function value in each epoch.
+	
+	"""
+	def __init__(self, eta=0.01, n_iter=50, random_state=1):
+		self.eta = eta
+		self.n_iter = n_iter
+		self.random_state = random_state
+
+	def fit(self, X, y):
+		"""Fit training data.
+
+		parameters
+		----------
+		X: {array-like}, shape = {n_samples, n_features}
+			Training vectors, where n_samples is the number of samples and
+			n_features is the number of features
+		y: array-like, shape = {n_samples}
+			Target values
+
+		Returns
+		-------
+		self: object
+
+		"""
+		rgen = np.random.RandomState(self.random_state)
+		self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+		self.cost_ = []
+
+		for i in range(self.n_iter):
+			net_input = self.net_input(X)
+			output = self.activation(net_input)
+			errors = (y - output)
+			self.w_[1:] += self.eta * X.T.dot(errors)
+			self.w_[0] += self.eta * errors.sum()
+			cost = (errors ** 2).sum() / 2.0
+			self.cost_.append(cost)
+		return self
+	
+	def net_input(self, X):
+		""" Calculate net input """
+		return np.dot(X, self.w_[1:]) + self.w_[0]
+
+	def activation(self, X):
+		""" Compute linear activation """
+		return X
+
+	def predict(self, X):
+		""" Return class label after unit step """
+		return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
+
 class Perceptron(object):
     """Perceptron classifier
 
@@ -72,8 +139,7 @@ class Perceptron(object):
 
 
 
-df = pd.read_csv('C:/Users/mimasaka/Documents/myOwnTraining/'
-                'Python ML/Python-Machine-Learning-Second-Edition/Chapter02/iris.data',
+df = pd.read_csv('C:/Users/mills/Documents/Python-Machine-Learning-Second-Edition/Chapter02/iris.data',
                 header = None)
 
 y = df.iloc[0:100, 4].values
@@ -124,10 +190,24 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
                     label=cl,
                     edgecolor='black')
 
-ppn = Perceptron(eta=0.1, n_iter=10)
-ppn.fit(X,y)
-plot_decision_regions(X, y, classifier=ppn)
-plt.xlabel('sepal length[cm]')
-plt.ylabel('petal length[cm]')
-plt.legend(loc='upper left')
+#ppn = Perceptron(eta=0.1, n_iter=10)
+#ppn.fit(X,y)
+#plot_decision_regions(X, y, classifier=ppn)
+#plt.xlabel('sepal length[cm]')
+#plt.ylabel('petal length[cm]')
+#plt.legend(loc='upper left')
+#plt.show()
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+adal = AdalineGD(n_iter=10, eta=0.01).fit(X,y)
+ax[0].plot(range(1, len(adal.cost_) + 1), np.log10(adal.cost_) + 1, marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X,y)
+ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squarred-error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
 plt.show()
