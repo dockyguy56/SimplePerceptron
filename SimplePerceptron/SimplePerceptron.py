0d3bd49514a500826297e9d11dda6c60d437b676
Miles Mbo <mimasaka@microsoft.com>
2019-08-19 16:35:26 -0700

    Chap 2 done

--- SimplePerceptron/SimplePerceptron.py
+++ SimplePerceptron/SimplePerceptron.py
@@ -71,6 +71,108 @@ class AdalineGD(object):
 		""" Return class label after unit step """
 		return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
 
+class AdalineSGD(object):
+    """ Adaptive LInear NEuron classifier.
+
+    Parameters
+    ----------
+    eta : float
+        Learning rate (between 0.0 and 1.0)
+    n_iter : int
+        Passes over the training dataset.
+    shuffle : bool (default:True)
+        Shuffles training data every epoch if True to prevent cycles.
+    randome_state : int
+        Random numbr generator seed for random weight initialization.
+
+    Attributes
+    ----------
+    w_ : 1d-array
+        Weights after fitting.
+    cost_ : list
+        Sum-of-squares cost function value averaged over all training samples
+        each epoxh.
+
+    """ 
+    def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = {n_samples, n_features}
+           Training vectors, where n_samples is the number of samples and
+           n_features is the number of featires.
+        y : array-like, shape = [n_samples]
+            Target values
+
+        Returns
+        -------
+        self : object
+        
+        """ 
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X,y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost) / len(y)
+            self.cost_.append(avg_cost)
+        return self
+
+    def partial_fit(self, X, y):
+            """ Fit training data without reinitializing the weights """
+            if not self.w_initialized:
+                self._initialize_weights(X.shape[1])
+            if y.ravel().shape[0] > 1:
+                for xi, target in zip(X, y):
+                    self._update_weights(xi, target)
+            else:
+                self._update_weights(X, y)
+            return self
+
+    def _shuffle(self, X, y):
+        """ Shuffle training data """
+        r = self.rgen.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        """ Initialize weights to small random numbers """
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc=0.0, scale=0.01, size=1 + m)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        """ Apply Adaline learning rule to update the weights """
+        output = self.activation(self.net_input(xi))
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error**2
+        cost = 0.5 * error**2
+        return cost
+
+    def net_input(self, X):
+        """ Calculate net input """
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        """ compute linear activation """
+        return X
+
+    def predict(self, X):
+        """ Return class label after unit step """
+        return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
+
 class Perceptron(object):
     """Perceptron classifier
 
@@ -159,9 +261,9 @@ X = df.iloc[0:100, [0,2]].values
 #plt.xlabel('Epochs')
 #plt.ylabel('Number of updates')
 #plt.show()
-
+    
 def plot_decision_regions(X, y, classifier, resolution=0.02):
-    """To visualize  the decisions boundaries
+    """ To visualize  the decisions boundaries
     """
     #setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
@@ -169,10 +271,22 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
     #plot the decision surface
+    #just find the limit and/reduce 1
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    #np.arange(start, stop, step): create list of tupple from start to stop with step of step
+    #np.meshgrid convert: create accessible arrays from list of tupple
+    #(-1,-2) (-1,0) (-1,1)    xx1 = [-1 -1 -1][0 0 0 ][1 1 1]
+    #(0,-2)(0,0)(0,1)      ==> 
+    #(1,-2)(1,0)(1,1)         xx2 = [-2 -2 -2][0 0 0 ][1 1 1]
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
                            np.arange(x2_min, x2_max, resolution))
+
+    #ravel() xx1 = [-1 -1 -1 0 0 0 1 1 1]
+    #        xx2 = [-2 -2 -2 0 0 0 1 1 1]
+    #array() [[-1 -1 -1 0 0 0 1 1 1]
+    #         [-2 -2 -2 0 0 0 1 1 1]] concatenation... sort of
+    #.T , transpose from in this case a 2x9 to 9x2
 
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
@@ -190,13 +304,6 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
                     label=cl,
                     edgecolor='black')
 
-#ppn = Perceptron(eta=0.1, n_iter=10)
-#ppn.fit(X,y)
-#plot_decision_regions(X, y, classifier=ppn)
-#plt.xlabel('sepal length[cm]')
-#plt.ylabel('petal length[cm]')
-#plt.legend(loc='upper left')
-#plt.show()
 
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
 adal = AdalineGD(n_iter=10, eta=0.01).fit(X,y)
@@ -210,4 +317,39 @@ ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
 ax[1].set_xlabel('Epochs')
 ax[1].set_ylabel('Sum-squarred-error')
 ax[1].set_title('Adaline - Learning rate 0.0001')
+plt.show()
+
+df = pd.read_csv('C:/Users/mimasaka/Documents/myOwnTraining/'
+            'Python ML/Python-Machine-Learning-Second-Edition/Chapter02/iris.data',
+            header = None)
+
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1,1)
+X = df.iloc[0:100, [0,2]].values
+
+
+ppn = Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X,y)
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('sepal length[cm]')
+plt.ylabel('petal length[cm]')
+plt.legend(loc='upper left')
+plt.show()
+
+X_std = np.copy(X)
+X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
+X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
+
+ada = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+ada.fit(X_std, y)
+
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Stochastic Gradient Decent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Average Cost')
 plt.show()
